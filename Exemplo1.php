849b6d7abc089b50f8812e1cb19373d00e2f9012
Ster <estefanemsantos@hotmail.com>
2017-05-06 20:27:27 -0300

    Add files via upload

---
+++ Exemplo1.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+ <meta charset="utf-8">
+ <title>Exemplo 01</title>
+</head>
+<body >
+<?php
+$idade=16;
+$nome="Stefany";
+echo "Olá meu nome é ",$nome,"<br> Minha idade é ",$idade;
+?>
+</center>
+</body>
+</html>
