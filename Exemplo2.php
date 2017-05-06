2ffaafbfcd8543f3e208623a4d69bb283e107826
Ster <estefanemsantos@hotmail.com>
2017-05-06 20:31:44 -0300

    Add files via upload

---
+++ Exemplo2.php
@@ -0,0 +1,16 @@
+<html>
+<Head>
+<title>Exemplo 02</title>
+</Head>
+<body bgcolor=#575815>
+<center>
+<?php
+echo "Constantes Declaradas<br>";
+define("CONSTANTE_ALO"," Olá turma! Assim que se declara uma contante!");
+echo CONSTANTE_ALO;
+echo "<br><br><br>---Constantes---";
+echo phpinfo();
+?>
+</center>
+</body>
+</html>
