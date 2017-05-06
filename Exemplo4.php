9ae4f269767f085c04d0b7ed985e27fa7ced2183
Ster <estefanemsantos@hotmail.com>
2017-05-06 20:38:11 -0300

    Add files via upload

---
+++ Exemplo4.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Exemplo 04</title>
+</head>
+<body >
+<?php
+echo"<h1>Operadores de Strings </h1>";
+$a= "Bem vinda";
+$b=" ao curso de";
+$c=" Informática!";
+$d= $a.$b.$c.="<br> Disciplina de PHP<br>";
+echo $d;
+
+?>
+
+</body>
+</html>
