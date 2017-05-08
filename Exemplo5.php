5b58b6d1b48f309e026326bdfe22a6262e841a32
Ster <estefanemsantos@hotmail.com>
2017-05-08 19:47:05 -0300

    Add files via upload

---
+++ Exemplo5.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+<title>Ster 2B</title>
+</head>
+<body >
+<center>
+<?php
+echo "<h1>Operadores de Comparação</h1><br>";
+$a=15;
+$b=45;
+$c=42.0;
+echo"$b == $c",$b==$c?" verdadeiro":" falso";
+echo"<br>";
+echo "$a === $c",$b===$c ?" verdadeiro" : " falso";
+echo "<br>";
+echo "$a != $c",$b!=$c ?" verdadeiro" : " falso";
+echo "<br>";
+echo " $a <> $c ",$b<>$c ?" verdadeiro" : " falso";
+echo "<br>";
+echo " $a < $c ",$b<$c ?" verdadeiro" : " falso";
+echo "<br>";
+echo "$a > $c",$b>$c ?" verdadeiro" : " falso";
+echo "<br>";
+echo "$a <= $c",$b<=$c ?" verdadeiro" : " falso";
+echo "<br>";
+echo "$a >= $c",$b>=$c ?" verdadeiro" : " falso";
+?>
+
+</center>
+</body>
+</html>
