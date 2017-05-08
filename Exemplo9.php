83147bd5cddf670120e528839b46668719831d44
Ster <estefanemsantos@hotmail.com>
2017-05-08 19:57:33 -0300

    Add files via upload

---
+++ Exemplo9.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>Ster 2B</title>
+</head>
+<body>
+<center>
+<h1>Estrutura de Repetição FOR</h1>
+	<?php
+for ($i=1; $i<= 10 ; $i++) {
+	echo "<b> Número: $i</b><br>";
+}
+?>
+</center>
+</body>
+</html>
