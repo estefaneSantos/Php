7d8b91a16976604bf76cbd1bcd5b07473c9f7073
Ster <estefanemsantos@hotmail.com>
2017-05-08 19:58:56 -0300

    Add files via upload

---
+++ Exemplo10.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Ster 2B</title>
+</head>
+<body>
+<center>
+<h1>Estrutura de Repetição WHILE</h1>
+	<?php
+ $num=1;
+ while ($num<=10) {
+	echo "<b> Número: $num</b><br>";
+	$num++;
+}
+?>
+</center>
+</body>
+</html>
