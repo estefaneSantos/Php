a5ddde07c5a239902ccf0ea8c4e1c81d9b315439
Ster <estefanemsantos@hotmail.com>
2017-05-08 19:51:43 -0300

    Add files via upload

---
+++ Exemplo7_2.php
@@ -0,0 +1,19 @@
+<html>
+<Head>
+<meta charset="utf-8"/>
+<title>Ster 2B</title>
+</Head>
+<body >
+<center>
+<h1>Estrutura Condicional(If/Else)</h1>
+<?php
+$idade=16;
+$maioridade=18;
+if($idade>=$maioridade){
+echo ("<br><b>$idade Anos,Você já é Maior de Idade!</b>");}
+else{
+echo ("<br><b>$idade Anos, Você Ainda não é maior de idade!</b>");}
+?>
+</center>
+</body>
+</html>
