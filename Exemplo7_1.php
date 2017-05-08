1088a9468fbb3c969665f3ad3608d6403065acbe
Ster <estefanemsantos@hotmail.com>
2017-05-08 19:50:22 -0300

    Add files via upload

---
+++ Exemplo7_1.php
@@ -0,0 +1,19 @@
+<html>
+<Head>
+<meta charset="utf-8"/>
+<TITLE>Ster 2B</TITLE>
+</Head>
+<body >
+<center>
+<?php
+$opc=2;
+if($opc==1){
+echo("<br><b>Opção escolhida 1</b>");
+}
+if($opc==2){
+echo("<h1><br><b>Opção escolhida 2</b></h1>");
+}
+?>
+</center>
+</body>
+</html>
