9f63e7390d43c105e074a415dac6dd1095a5d1f1
Ster <estefanemsantos@hotmail.com>
2017-05-08 20:05:07 -0300

    Add files via upload

---
+++ Exemplo12.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Ster 2B</title>
+</head>
+<body>
+<center>
+<?php
+function msn(){
+$msn="Olá Bem Vindo as Funções de PHP";
+ echo"<h2> $msn</h2>";
+ }
+ function imprimir($texto){
+ echo"<h3>$texto</h3>";
+ }
+ msn();
+ imprimir("testando função");
+
+?>
+</center>
+</body>
+</html>
