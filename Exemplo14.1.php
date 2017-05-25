f73bff28bb49185dfdbab80d6a94d5e2171c66bd
Ster <estefanemsantos@hotmail.com>
2017-05-24 21:14:10 -0300

    Add files via upload

---
+++ Exemplo14.1.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+<title>Ster 2B</title>
+</head>
+<body>
+<?php
+function testeLocal(){
+$a=25;
+}
+echo"<h1>Variável Local</h1>";
+testeLocal();
+echo $a=25;
+?>
+</body>
+</html>
