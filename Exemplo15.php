3bd013c3641a44caf469747e5066c717ea293c3c
Ster <estefanemsantos@hotmail.com>
2017-05-24 21:58:19 -0300

    Add files via upload

---
+++ Exemplo15.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+<title>Ste 2B</title>
+</head>
+<body>
+<?php
+function mais5($numero){
+$numero += 5;
+}
+echo"<h1>Passagem de Parâmetro</h1>";
+$a=3;
+mais5($a);
+echo"Valor da Variável a = $a <br>";
+?>
+</body>
+</html>
