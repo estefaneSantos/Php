a3e574bee02d2cb2ae9529a0b7323fe089861490
Ster <estefanemsantos@hotmail.com>
2017-05-24 22:02:59 -0300

    Add files via upload

---
+++ Exemplo16.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+<title>Ster 2B</title>
+</head>
+<body>
+<h1>Exemplo 16</h1>
+<?php
+function teste($txt = "Testando"){
+echo"<h2> $txt </h2>";
+}
+teste();
+teste("Outro Teste");
+?>
+</body>
+</html>
